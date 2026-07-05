dab736d4c8559c99b9516e46469e01bab2984be9
Gökhan Türk <ensar.gokhan@hotmail.com>
2026-07-06 00:53:44 +0300

    fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,16 @@
-source /usr/share/cachyos-fish-config/*
-abbr -a cls clear
-zoxide init fish | source
+function source_if_exists
+    if test -f $argv[1]
+	source $argv[1]
+    end
+end
+
+source_if_exists /usr/share/cachyos-fish-config/*
+
+if command -q zoxide
+    zoxide init fish | source
+end
+
 set -gx TERMINAL "alacritty -e"
+
+abbr -a cls clear
+abbr -a ff fastfetch
